e099870138b865406707f59397830a5453cdb735
Vipul <vipulkumar984489@gmail.com.com>
2026-09-13 22:16:39 +0530

    creating the collector to monitor hot tables

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	agentsinternal "github.com/Vipul984/pg-monitor/internal/AgentsInternal"
+	"github.com/Vipul984/pg-monitor/internal/collector"
 )
 
 func main() {
@@ -23,6 +24,10 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
- 	agentRun := agentsinternal.NewAgentRun(pool)
+ 	collectors := []collector.Collector{
+		collector.NewHotTablesCollector(pool),
+	}
+
+	agentRun := agentsinternal.NewAgentRun(pool, collectors)
 	agentRun.RunAgent(ctx)
 }
